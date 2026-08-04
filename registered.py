e4ac3ae99ec77563c9c10ab2d96a6847b335ae01
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-08-03 20:15:38 -0400

    Add synthesized implementation generated in workbench

--- registered.py
+++ registered.py
@@ -1,6 +1,2 @@
-# Comment added from workbench and submitted through it in workbench terminal
-
-# Second comment
-
-def test():
-    print("test!")
+def getTerminalInput(prompt):
+    return input(prompt)
