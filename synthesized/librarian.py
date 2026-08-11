afa4b5ecd039ccb6fc37f74d77700baf47e1619d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-08-10 21:29:34 -0400

    Update synthesized output that has invariants before next behavior is selected.

--- synthesized/librarian.py
+++ synthesized/librarian.py
@@ -5,38 +5,34 @@ from LoggingHelper import semanticLogger
 
 def createBasket():
     worldStateManager.setBehavior('createBasket')
-    global worldState
-    if True:
-        print(f'Invariant for transformation _insertIntoList in behavior addBookToBasket')
-    if True:
-        print(f'Invariant for transformation _removeFromPosition in behavior getBookFromBasket')
-    if True:
-        print(f'Invariant for transformation _getFromPosition in behavior getBookFromBasket')
     basket = []
     worldStateManager.create('basket', basket, 'BASKET', 'BOOK_BASKET', False)
+    if True:
+        print(f'Invariant for basket in transformation _getFromPosition in behavior getBookFromBasket')
+    if True:
+        print(f'Invariant for basket in transformation _removeFromPosition in behavior getBookFromBasket')
+    if True:
+        print(f'Invariant for basket in transformation _insertIntoList in behavior addBookToBasket')
     return 'getChoice'
 
 def getChoice():
     worldStateManager.setBehavior('getChoice')
-    global worldState
-    if True:
-        print(f'Invariant for transformation _isEqual in behavior evaluateChoice')
-    if True:
-        print(f'Invariant for transformation _isEqual in behavior evaluateChoice')
     choice = input('\nGet user choice (a for add book, g for get book, else exit): ')
     worldStateManager.create('choice', choice, 'CHOICE', 'USER_CHOICE', True)
+    if True:
+        print(f'Invariant for choice in transformation _isEqual in behavior evaluateChoice')
+    if True:
+        print(f'Invariant for choice in transformation _isEqual in behavior evaluateChoice')
     return 'displayChoice'
 
 def displayChoice():
     worldStateManager.setBehavior('displayChoice')
-    global worldState
     choice = worldStateManager.getValue('choice', 'CHOICE', 'USER_CHOICE')
     print(f'User Choice: {choice}')
     return 'evaluateChoice'
 
 def evaluateChoice():
     worldStateManager.setBehavior('evaluateChoice')
-    global worldState
     choice = worldStateManager.getValue('choice', 'CHOICE', 'USER_CHOICE')
     isAdd = isEqual(choice, 'a')
     isGet = isEqual(choice, 'g')
@@ -47,17 +43,14 @@ def evaluateChoice():
 
 def addBookChoice():
     worldStateManager.setBehavior('addBookChoice')
-    global worldState
     return 'getName'
 
 def getBookChoice():
     worldStateManager.setBehavior('getBookChoice')
-    global worldState
     return 'getBookFromBasket'
 
 def getBookFromBasket():
     worldStateManager.setBehavior('getBookFromBasket')
-    global worldState
     basket = worldStateManager.getValue('basket', 'BASKET', 'BOOK_BASKET')
     book = getFromPosition(basket, 0)
     removeFromPosition(basket, 0)
@@ -66,7 +59,6 @@ def getBookFromBasket():
 
 def getBookName():
     worldStateManager.setBehavior('getBookName')
-    global worldState
     book = worldStateManager.getValue('book', 'BOOK', 'BOOK_W_NAME')
     name = book['name']
     worldStateManager.add('name', name, 'NAME', 'BOOK_NAME', False)
@@ -74,7 +66,6 @@ def getBookName():
 
 def getFirstLetterOfBookName():
     worldStateManager.setBehavior('getFirstLetterOfBookName')
-    global worldState
     name = worldStateManager.getValue('name', 'NAME', 'BOOK_NAME')
     firstLetter = getFromPosition(name, 0)
     print(f'Got book named {name} and it has first letter {firstLetter}')
@@ -83,28 +74,25 @@ def getFirstLetterOfBookName():
 
 def getName():
     worldStateManager.setBehavior('getName')
-    global worldState
-    if True:
-        print(f'Invariant for transformation _getFromPosition in behavior getFirstLetterOfBookName')
     name = input('\nPlease enter book name: ')
     worldStateManager.create('name', name, 'NAME', 'BOOK_NAME', True)
+    if True:
+        print(f'Invariant for name in transformation _getFromPosition in behavior getFirstLetterOfBookName')
     return 'createBook'
 
 def createBook():
     worldStateManager.setBehavior('createBook')
-    global worldState
-    if True:
-        print(f'Invariant for transformation _insertIntoList in behavior addBookToBasket')
     name = worldStateManager.getValue('name', 'NAME', 'BOOK_W_NAME')
     book = {}
     book['name'] = name
     worldStateManager.create('book', book, 'BOOK', 'BOOK_W_NAME', False)
     worldStateManager.remove('name')
+    if True:
+        print(f'Invariant for book in transformation _insertIntoList in behavior addBookToBasket')
     return 'addBookToBasket'
 
 def addBookToBasket():
     worldStateManager.setBehavior('addBookToBasket')
-    global worldState
     book = worldStateManager.get('book', 'BOOK', 'BOOK_W_NAME')
     basket = worldStateManager.getValue('basket', 'BASKET', 'BOOK_BASKET')
     basket = insertIntoList(basket, book, 0)
@@ -114,7 +102,6 @@ def addBookToBasket():
 
 def showBasket():
     worldStateManager.setBehavior('showBasket')
-    global worldState
     basket = worldStateManager.getValue('basket', 'BASKET', 'BOOK_BASKET')
     print(f'Basket Contents: {basket}')
     return 'getChoice'
