714affae955940d443303c9e6a7c1c95b60e5979
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-08-11 00:12:45 -0400

    Add call if exist method.

---
+++ registered.py
@@ -0,0 +1,10 @@
+from computable_units import *
+
+def callIfExist(method_name, *args, **kwargs):
+    method = globals().get(method_name)
+    print(method_name)
+
+    if callable(method):
+        return method(*args, **kwargs)
+    else:
+        return None
